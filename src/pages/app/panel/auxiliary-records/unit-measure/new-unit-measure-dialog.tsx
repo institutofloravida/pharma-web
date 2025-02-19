0c973681b18eb1135fdf506ee3ccbb8546237615
yuri <yurisousaenfer@gmail.com>
2025-02-18 21:39:38 -0300

    refactor(unit measure): create form

--- src/pages/app/panel/auxiliary-records/unit-measure/new-unit-measure-dialog.tsx
+++ src/pages/app/panel/auxiliary-records/unit-measure/new-unit-measure-dialog.tsx
@@ -12,6 +12,7 @@ import {
   DialogClose,
   DialogContent,
   DialogDescription,
+  DialogFooter,
   DialogHeader,
   DialogTitle,
 } from '@/components/ui/dialog'
@@ -27,6 +28,7 @@ import { Input } from '@/components/ui/input'
 import { useAuth } from '@/contexts/authContext'
 import { toast } from '@/hooks/use-toast'
 import { queryClient } from '@/lib/react-query'
+import { handleApiError } from '@/lib/utils/handle-api-error'
 
 const FormSchema = z.object({
   name: z
@@ -43,24 +45,16 @@ const FormSchema = z.object({
     .max(10, { message: 'O campo deve ter no máximo 10 caracteres' }),
 })
 
-type NewUnitMeasureSchema = z.infer<typeof FormSchema>
-
 export function NewUnitMeasureDialog() {
   const { token } = useAuth()
 
   const { mutateAsync: registerTherapeuticClassFn } = useMutation({
     mutationFn: (data: RegisterUnitMeasureBody) =>
       registerUnitMeasure(data, token ?? ''),
-    onSuccess(_, { name, acronym }) {
-      const cached =
-        queryClient.getQueryData<NewUnitMeasureSchema[]>(['units-measure']) ||
-        []
-      if (cached) {
-        queryClient.setQueryData(
-          ['units-measure'],
-          [{ name, acronym }, ...cached],
-        )
-      }
+    onSuccess() {
+      queryClient.invalidateQueries({
+        queryKey: ['units-measure'],
+      })
     },
   })
 
@@ -79,16 +73,11 @@ export function NewUnitMeasureDialog() {
         title: 'Unidade de Medida cadastrada com sucesso!',
       })
     } catch (error) {
+      const errorMessage = handleApiError(error)
       toast({
-        title: 'Error ao cadastrar a unidade de medida',
+        title: 'Erro ao tentar atualizar a unidade de medida.',
+        description: errorMessage,
         variant: 'destructive',
-        description: (
-          <pre className="mt-2 w-[340px] rounded-md bg-slate-950 p-4">
-            <code className="text-white">
-              {JSON.stringify(error.message, null, 2)}
-            </code>
-          </pre>
-        ),
       })
     }
   }
@@ -104,13 +93,13 @@ export function NewUnitMeasureDialog() {
       <Form {...form}>
         <form
           onSubmit={form.handleSubmit(onSubmit)}
-          className="space-y-auto w-[80%] space-x-0 space-y-6"
+          className="grid grid-cols-3 space-y-2"
         >
           <FormField
             control={form.control}
             name="name"
             render={({ field }) => (
-              <FormItem>
+              <FormItem className="col-span-3">
                 <FormLabel>Nome</FormLabel>
                 <FormControl>
                   <Input placeholder="Unidade de medida..." {...field} />
@@ -124,7 +113,7 @@ export function NewUnitMeasureDialog() {
             control={form.control}
             name="acronym"
             render={({ field }) => (
-              <FormItem>
+              <FormItem className="col-span-3">
                 <FormLabel>Abreviação</FormLabel>
                 <FormControl>
                   <Input placeholder="Abreviação..." {...field} />
@@ -133,12 +122,16 @@ export function NewUnitMeasureDialog() {
               </FormItem>
             )}
           />
-          <div className="flex items-center justify-end gap-2">
-            <DialogClose asChild>
-              <Button variant={'outline'}>Cancelar</Button>
-            </DialogClose>
-            <Button type="submit">Submit</Button>
-          </div>
+          <DialogFooter className="col-span-3 grid justify-end">
+            <div className="flex gap-2">
+              <DialogClose asChild>
+                <Button variant={'ghost'}>Cancelar</Button>
+              </DialogClose>
+              <Button type="submit" disabled={form.formState.isSubmitting}>
+                Salvar
+              </Button>
+            </div>
+          </DialogFooter>
         </form>
       </Form>
     </DialogContent>
