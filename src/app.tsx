b467a6f984718d0efe6bf34438140bf216db1d1f
yuri <yurisousaenfer@gmial.com>
2026-03-09 09:28:44 -0300

    feat: show message sucessfully on save settings

--- src/app.tsx
+++ src/app.tsx
@@ -3,8 +3,7 @@ import { Helmet, HelmetProvider } from "react-helmet-async";
 import { RouterProvider } from "react-router-dom";
 
 import { ThemeProvider } from "./components/theme/theme-provider";
-import { Toast } from "./components/ui/toast";
-import { Toaster } from "./components/ui/toaster";
+import { Toaster } from "./components/ui/sonner";
 import { queryClient } from "./lib/react-query";
 import { router } from "./routes";
 
@@ -13,8 +12,7 @@ export function App() {
     <HelmetProvider>
       <ThemeProvider storageKey="pharma-theme" defaultTheme="dark">
         <Helmet titleTemplate="%s | FarmaVida" />
-        {/* <Toaster richColors closeButton position="top-right" /> */}
-        <Toaster />
+        <Toaster richColors closeButton position="top-right" />
         <RouterProvider router={router} />
       </ThemeProvider>
     </HelmetProvider>
